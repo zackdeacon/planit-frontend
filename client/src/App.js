9a466ebf563d698b37472ca8173264c4ef530db8
Zachary Deacon <zackdeacon347@gmail.com>
2020-08-13 14:20:53 -0700

    adding react app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from "./pages/Home/Home"
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
