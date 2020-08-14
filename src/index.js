ffa9819eee569015da32c5443718960239235604
Vinny V <VinAVarghese@gmail.com>
2020-08-14 10:19:24 -0700

    landing component finished

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Chat from "../src/components/Chat/chat"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Chat />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
