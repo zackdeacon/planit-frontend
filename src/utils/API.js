d17ad00db4eb8bf95ed4c11569ec166894cdfd85
Derek Watson <derek.watson92@gmail.com>
2020-08-15 11:43:46 -0700

    chat route, API.js update

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+const urlPrefix = "http://localhost:8080";
 
 // ** Functions that take an object have the structure and keys of that 
 // ** object laid out in a comment within!
@@ -7,83 +8,83 @@ export default {
   // * AUTHORIZATION
   signup: function (userData) {
     // userData: { username, password, email, name: {first, last}}
-    return axios.post("/api/signup", userData);
+    return axios.post(`${urlPrefix}/api/auth/signup`, userData, { withCredentials: true });
   },
   login: function (loginData) {
     // loginData: { username, password }
-    return axios.post("/api/login", loginData);
+    return axios.post(`${urlPrefix}/api/auth/login`, loginData, { withCredentials: true });
   },
   logout: function () {
-    return axios.get("api/logout");
+    return axios.get(`${urlPrefix}/api/auth/logout`, { withCredentials: true });
   },
-  readSessions: function () {
-    return axios.get("api/readsessions");
+  getSessionData: function () {
+    return axios.get(`${urlPrefix}/api/auth/readsession`, { withCredentials: true });
   },
 
   // * USERS COLLECTION
   getAllUsers: function () {
-    return axios.get("api/users");
+    return axios.get(`${urlPrefix}/api/users`);
   },
   getUserById: function (user) {
     // user: { id }
-    return axios.get("api/users/one/id", user);
+    return axios.get(`${urlPrefix}/api/users/one/id`, user);
   },
   getUserByUsername: function (user) {
     // user: { username }
-    return axios.get("api/users/one/username", user);
+    return axios.get(`${urlPrefix}/api/users/one/username`, user);
   },
   deleteUser: function (user) {
     // user: { id }
-    return axios.delete("api/users/delete", user);
+    return axios.delete(`${urlPrefix}/api/users/delete`, user);
   },
 
   // * MAPS COLLECTION
   getAllMaps: function () {
-    return axios.get("/api/maps");
+    return axios.get(`${urlPrefix}/api/maps`);
   },
   getMapById: function (map) {
     // map: { id }
-    return axios.get("/api/maps/one/id", map);
+    return axios.get(`${urlPrefix}/api/maps/one/id`, map);
   },
   postNewMap: function (mapData) {
     // mapData: { name, creatorId, dates: {start, end}, destinations }
     // Note: dates and destinations keys are optional in above object
-    return axios.post("/api/maps/new", mapData);
+    return axios.post(`${urlPrefix}/api/maps/new`, mapData);
   },
   deleteMap: function (map) {
     // map: { id }
-    return axios.delete("/api/maps/delete", map);
+    return axios.delete(`${urlPrefix}/api/maps/delete`, map);
   },
 
   // * SUGGESTIONS COLLECTION
   getAllSuggestions: function () {
-    return axios.get("/api/suggestions");
+    return axios.get(`${urlPrefix}/api/suggestions`);
   },
   postNewSuggestion: function (suggestionData) {
     // suggestionData: { userId, mapId, title, category, description, link, cost }
     // Note: link and cost keys are optional in above object
-    return axios.post("/api/suggestions/new", suggestionData);
+    return axios.post(`${urlPrefix}/api/suggestions/new`, suggestionData);
   },
   // Saves a suggestion to the database
   deleteSuggestion: function (suggestion) {
     // suggestion: { id }
-    return axios.post("/api/suggestions/delete", suggestion);
+    return axios.post(`${urlPrefix}/api/suggestions/delete`, suggestion);
   },
 
   // * CHATS COLLECTION
   getAllChats: function () {
-    return axios.get("/api/chats");
+    return axios.get(`${urlPrefix}/api/chats`);
   },
   getChatsFromMap: function (map) {
     // map: { id }
-    return axios.get("/api/chats/map", map);
+    return axios.get(`${urlPrefix}/api/chats/map`, map);
   },
   postNewChat: function (chatData) {
     // chatData: { userId, mapId, message }
-    return axios.post("/api/chats/new", chatData);
+    return axios.post(`${urlPrefix}/api/chats/new`, chatData);
   },
   deleteChat: function (chat) {
     // chat: { id }
-    return axios.post("/api/chats/delete", chat);
+    return axios.post(`${urlPrefix}/api/chats/delete`, chat);
   }
 };
