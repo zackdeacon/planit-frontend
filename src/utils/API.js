f5502e908df2d1c6178ce1867b2e553c77902d84
Derek Watson <derek.watson92@gmail.com>
2020-08-15 11:04:02 -0700

    added all current routes to utils/API.js

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,58 +1,89 @@
 import axios from "axios";
 
+// ** Functions that take an object have the structure and keys of that 
+// ** object laid out in a comment within!
+
 export default {
-  // AUTHORIZATION
-  signup: function () {
+  // * AUTHORIZATION
+  signup: function (userData) {
+    // userData: { username, password, email, name: {first, last}}
     return axios.post("/api/signup", userData);
   },
-  signup: function () {
-    return axios.post("/api/signup");
+  login: function (loginData) {
+    // loginData: { username, password }
+    return axios.post("/api/login", loginData);
+  },
+  logout: function () {
+    return axios.get("api/logout");
+  },
+  readSessions: function () {
+    return axios.get("api/readsessions");
   },
 
-  //MAPS
-  // Gets all maps
-  getMaps: function () {
+  // * USERS COLLECTION
+  getAllUsers: function () {
+    return axios.get("api/users");
+  },
+  getUserById: function (user) {
+    // user: { id }
+    return axios.get("api/users/one/id", user);
+  },
+  getUserByUsername: function (user) {
+    // user: { username }
+    return axios.get("api/users/one/username", user);
+  },
+  deleteUser: function (user) {
+    // user: { id }
+    return axios.delete("api/users/delete", user);
+  },
+
+  // * MAPS COLLECTION
+  getAllMaps: function () {
     return axios.get("/api/maps");
   },
-  // Gets the map with the given id
-  getMap: function (id) {
-    return axios.get("/api/maps/" + id);
+  getMapById: function (map) {
+    // map: { id }
+    return axios.get("/api/maps/one/id", map);
   },
-  // Deletes the map with the given id
-  deleteMap: function (id) {
-    return axios.delete("/api/maps/" + id);
-  },
-  // Saves a map to the database
-  saveMap: function (mapData) {
+  postNewMap: function (mapData) {
+    // mapData: { name, creatorId, dates: {start, end}, destinations }
+    // Note: dates and destinations keys are optional in above object
     return axios.post("/api/maps/new", mapData);
   },
+  deleteMap: function (map) {
+    // map: { id }
+    return axios.delete("/api/maps/delete", map);
+  },
 
-  //SUGGESTIONS
-  // Gets all suggesstions
-  getSuggestions: function () {
+  // * SUGGESTIONS COLLECTION
+  getAllSuggestions: function () {
     return axios.get("/api/suggestions");
   },
-  // Gets the suggestion with the given id
-  getSuggestion: function (id) {
-    return axios.get("/api/suggestions/" + id);
-  },
-  // Deletes the suggestion with the given id
-  deleteSuggestion: function (id) {
-    return axios.delete("/api/suggestions/" + id);
-  },
-  // Saves a suggestion to the database
-  saveSuggestion: function (suggestionData) {
+  postNewSuggestion: function (suggestionData) {
+    // suggestionData: { userId, mapId, title, category, description, link, cost }
+    // Note: link and cost keys are optional in above object
     return axios.post("/api/suggestions/new", suggestionData);
   },
+  // Saves a suggestion to the database
+  deleteSuggestion: function (suggestion) {
+    // suggestion: { id }
+    return axios.post("/api/suggestions/delete", suggestion);
+  },
 
-  //Chats
-  getAllChats: function() {
+  // * CHATS COLLECTION
+  getAllChats: function () {
     return axios.get("/api/chats");
   },
-  postChat: function(chatData) {
-    return axios.post("/api/chats/new", chatData)
+  getChatsFromMap: function (map) {
+    // map: { id }
+    return axios.get("/api/chats/map", map);
   },
-  getChatsFromMap: function(mapId) {
-    return axios.get("/api/chats/map", mapId);
+  postNewChat: function (chatData) {
+    // chatData: { userId, mapId, message }
+    return axios.post("/api/chats/new", chatData);
   },
+  deleteChat: function (chat) {
+    // chat: { id }
+    return axios.post("/api/chats/delete", chat);
+  }
 };
