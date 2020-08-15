f40bb60a2f6d97ebfe5f4d8fc7bdff575a571ec5
Derek Watson <derek.watson92@gmail.com>
2020-08-15 10:31:57 -0700

    started adding the real routes

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,39 +1,47 @@
 import axios from "axios";
 
 export default {
+  // AUTHORIZATION
+  signup: function () {
+    return axios.post("/api/signup", userData);
+  },
+  signup: function () {
+    return axios.post("/api/signup");
+  },
+
   //MAPS
   // Gets all maps
-  getMaps: function() {
+  getMaps: function () {
     return axios.get("/api/maps");
   },
   // Gets the map with the given id
-  getMap: function(id) {
+  getMap: function (id) {
     return axios.get("/api/maps/" + id);
   },
   // Deletes the map with the given id
-  deleteMap: function(id) {
+  deleteMap: function (id) {
     return axios.delete("/api/maps/" + id);
   },
   // Saves a map to the database
-  saveMap: function(mapData) {
+  saveMap: function (mapData) {
     return axios.post("/api/maps/new", mapData);
   },
 
   //SUGGESTIONS
   // Gets all suggesstions
-  getSuggestions: function() {
+  getSuggestions: function () {
     return axios.get("/api/suggestions");
   },
   // Gets the suggestion with the given id
-  getSuggestion: function(id) {
+  getSuggestion: function (id) {
     return axios.get("/api/suggestions/" + id);
   },
   // Deletes the suggestion with the given id
-  deleteSuggestion: function(id) {
+  deleteSuggestion: function (id) {
     return axios.delete("/api/suggestions/" + id);
   },
   // Saves a suggestion to the database
-  saveSuggestion: function(suggestionData) {
+  saveSuggestion: function (suggestionData) {
     return axios.post("/api/suggestions/new", suggestionData);
   }
 };
