d443e194cdfe5d53a411a0e7a6f1358c59c1db20
Zachary Deacon <zackdeacon347@gmail.com>
2020-08-17 20:21:47 -0700

    final render structure done

---
+++ src/pages/FinalRender/FinalRender.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from '../../components/NavBar/navbar'
+import FinalRenderCard from '../../components/FinalRenderCard/FinalRenderCard'
+// import Chat from '../../'
+// import './mapdashboard.css'
+
+export default function FinalRender() {
+    return (
+        <div className="dash-background">
+        <div className="dash-filter-background">
+        <NavBar logo="./assets/logos/logotxt.png" width="80px" left="-40px" top="10px"/>
+        <FinalRenderCard />
+        </div>
+        </div>
+    )
+}
