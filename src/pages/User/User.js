aec9d6c823402375b381c088147e022c74304042
Bryce Pingul <brycepingul@gmail.com>
2020-08-18 00:02:29 -0700

    adding logic for to retrieve current user info

--- src/pages/User/User.js
+++ src/pages/User/User.js
@@ -7,26 +7,37 @@ import MapCarousel from '../../components/MapCarousel/MapCarousel';
 
 export default function User() {
     const [maps, setMaps] = useState([])
+    const [user, setUser] = useState({
+        username: "",
+        email: ""
+    })
 
     useEffect(() => {
-        API.getAllMaps().then(data => {
-            console.log('data', data)
-            const prunedMaps = data.data.map(item => {
-              return{
-                  name: item.name,
-                  _id: item._id
-              }
-            } )
-            setMaps(prunedMaps)
-        }
-        )
+        // API.getUserByUsername(req.session.username).then(result => {
+        //     const currentUserInfo = {
+        //         username: result.username,
+        //         email: result.email
+        //     } 
+        //     setUser(currentUserInfo)
+            API.getAllMaps().then(data => {
+                console.log('data', data)
+                const prunedMaps = data.data.map(item => {
+                    return {
+                        name: item.name,
+                        _id: item._id
+                    }
+                })
+                setMaps(prunedMaps)
+            })
             .catch(err => console.log('err', err))
-    }, [])
+        // })
+        // .catch(err => console.log('err', err))
+}, [])
 
-    return (
-        <>
-            <NavBar logo="./assets/logos/logotxt.png" width="80px" left="-40px" top="10px" />
-            <UserCard maps={maps}/>
-        </>
-    )
+return (
+    <>
+        <NavBar logo="./assets/logos/logotxt.png" width="80px" left="-40px" top="10px" />
+        <UserCard maps={maps} user={user}/>
+    </>
+)
 }
